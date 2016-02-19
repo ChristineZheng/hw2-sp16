78980c286ab99f5e64db5e35585aa98737a4c65b
Guomin Zheng <ChristineZheng@calvisitor-10-105-159-66.calvisitor.1918.berkeley.edu>
2016-02-19 13:37:33 -0800

    whatever message you want

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,15 @@
 class Foobar
+	@foo 
+
+	def initialize(input)
+		@second = input
+	end
+
+	def bar(argument1, argument2={})
+		first = argument1
+		third = argument2[:sat]
+		"#{first}#{@second}#{third}"
+	end
+
   # ENTER CODE FOR Q2 HERE
 end
